cb9b5fa83ebfb76e573d24b3b5c3995c9a47b008
RoamingNoMaD <janosl.vasik@gmail.com>
2022-10-04 08:56:12 +0200

    Add initialization of reallocated memory

--- src/scanner.c
+++ src/scanner.c
@@ -9,6 +9,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define initSize 16
 #define growth 8
@@ -37,6 +38,7 @@ string_t string_init(){
 }
 
 bool resize(string_t currString){
+    size_t oldSize = currString->memSize;
     if((currString->currLen+1) == currString->memSize){
         currString->memSize = currString->memSize + growth;
         void* data = realloc(currString, currString->memSize * sizeof(char));
@@ -47,6 +49,8 @@ bool resize(string_t currString){
             currString->data = data;
         }
     }
+
+    memset(currString->data + oldSize, 0, oldSize); // initializing reallocated memory
     return true;
 }
 
