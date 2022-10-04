3f0df8efe8d44a0aac77f9fde52197ec35a7d667
RoamingNoMaD <janosl.vasik@gmail.com>
2022-10-04 08:12:49 +0200

    Add string_init

--- src/scanner.c
+++ src/scanner.c
@@ -7,6 +7,10 @@
 #include "scanner.h"
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <bool.h>
+
+#define initSize 32
 
 struct String
 {
@@ -14,3 +18,19 @@ struct String
     size_t currLen;
     size_t memSize;
 };
+
+string_t string_init(){
+    string_t currString = calloc(1, sizeof(struct String));
+    if(currString == NULL){
+        return NULL;
+    }
+
+    currString->data = calloc(initSize, sizeof(char));
+    if(currString->data == NULL){
+        free(currString);
+        return NULL;
+    }
+    currString->memSize = initSize;
+    
+    return true;
+}
