6fc78c67d6d5bbb514379ca59d65f96eba97c5ae
log <andrejsmatana0@gmail.com>
2022-10-07 21:28:46 +0200

    Change value to attribute

--- tests/scan_test.c
+++ tests/scan_test.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "../src/scanner.h"
 
 int main() {
@@ -10,39 +11,39 @@ int main() {
         switch (token->type) {
             //////////////////PROLOGUE//////////////////////////
             case TOK_PROLOGUE:
-                printf("TOK_PROLOGUE: %s)", token->value); /* <? */
+                printf("TOK_PROLOGUE: %s)", token->attribute); /* <? */
                 break;
             case TOK_KEY_ID:
-                printf("TOK_KEY_ID: %s)", token->value); /* php */ // (or other keywords ?? todo)
+                printf("TOK_KEY_ID: %s)", token->attribute); /* php */ // (or other keywords ?? todo)
                 break;
             case TOK_END_SIGN:
-                printf("TOK_END_SIGN: %s)", token->value); /* ?> */
+                printf("TOK_END_SIGN: %s)", token->attribute); /* ?> */
                 break;
             ///////////////////NUMBERS//////////////////////////
             case TOK_INT_LIT:
-                printf("TOK_INT_LIT: %d)", token->value); /* 123 */
+                printf("TOK_INT_LIT: %d)", token->attribute); /* 123 */
                 break;
             case TOK_DEC_LIT:
-                printf("TOK_DEC_LIT: %f)", token->value); /* 123.456 */
+                printf("TOK_DEC_LIT: %f)", token->attribute); /* 123.456 */
                 break;
             ///////////////////KEYWORDS//////////////////////////
             case TOK_WHILE:
-                printf("TOK_WHILE: %s)", token->value); /* while */
+                printf("TOK_WHILE: %s)", token->attribute); /* while */
                 break;
             case TOK_IF:
-                printf("TOK_IF: %s)", token->value); /* if */
+                printf("TOK_IF: %s)", token->attribute); /* if */
                 break;
             case TOK_VOID:
-                printf("TOK_VOID: %s)", token->value); /* void */
+                printf("TOK_VOID: %s)", token->attribute); /* void */
                 break;
             case TOK_INT:
-                printf("TOK_INT: %s)", token->value); /* int */
+                printf("TOK_INT: %s)", token->attribute); /* int */
                 break;
             case TOK_FALSE:
-                printf("TOK_FALSE: %s)", token->value); /* false */
+                printf("TOK_FALSE: %s)", token->attribute); /* false */
                 break;
             case TOK_ELSE:
-                printf("TOK_ELSE: %s)", token->value); /* else */
+                printf("TOK_ELSE: %s)", token->attribute); /* else */
                 break;
             default:
                 break;
@@ -53,4 +54,6 @@ int main() {
             printf("ERROR");
         }
     }
+
+    return 0;
 }
