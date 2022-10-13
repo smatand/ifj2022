ac587cd99ed58a22cdf59686d662588f95f85eed
tomasfratrik <tomasfratrik8@gmail.com>
2022-10-13 16:03:33 +0200

    temporary stdin scanner for testing expressions

--- src/expr_test.c
+++ src/expr_test.c
@@ -21,77 +21,87 @@ eItem_t *findClosestTerm(eStack_t *stack){
     }
     return currItem;
 }
+char ownScanner(){
+    int c = fgetc(stdin);
+    return c;
+}
+
+int charToToken(char s){
+    switch(s){
+        case 'i':
+            return TOK_INT_LIT;
+        case '+':
+            return TOK_PLUS;
+        case '-':
+            return TOK_MINUS;
+        case '*':
+            return TOK_STAR;
+        case '(':
+            return TOK_LEFT_PAREN;
+        case ')':
+            return TOK_RIGHT_PAREN;
+        case '$':
+            return 100;
+    }
+}
+
 int main(){
     bool continueParsing = true;
 	eStack_t estack;
 	eStack_t *stack = &estack;
 	eStackInit(stack);
 	eStackPushDollar(stack);
-    eStack_t expr1;
-    eStack_t *expr = &expr1;
-    eStackInit(expr);
 
 
-    //tokens for (i+i)*i
-    token_t *token1 = myTokenInit(TOK_INT_LIT); //1i
-    token_t *token2 = myTokenInit(TOK_STAR); //1i
-    token_t *token3 = myTokenInit(TOK_RIGHT_PAREN); //1i
-    token_t *token4 = myTokenInit(TOK_INT_LIT); //1i
-    token_t *token5 = myTokenInit(TOK_PLUS); //1i
-    token_t *token6 = myTokenInit(TOK_INT_LIT); //1i
-    token_t *token7 = myTokenInit(TOK_LEFT_PAREN); //1i
-    eItem_t *item1 = eItemInit(token1, TERM);
-    eItem_t *item2 = eItemInit(token2, TERM);
-    eItem_t *item3 = eItemInit(token3, TERM);
-    eItem_t *item4 = eItemInit(token4, TERM);
-    eItem_t *item5 = eItemInit(token5, TERM);
-    eItem_t *item6 = eItemInit(token6, TERM);
-    eItem_t *item7 = eItemInit(token7, TERM);
-    eStackPushDollar(expr);
-    eStackPushItem(expr,item1);
-    eStackPushItem(expr,item2);
-    eStackPushItem(expr,item3);
-    eStackPushItem(expr,item4);
-    eStackPushItem(expr,item5);
-    eStackPushItem(expr,item6);
-    eStackPushItem(expr,item7);
 
+
+
+    int incomingTokenType;
     int stackTokenType;
-    int currItemToken;
-    eItem_t *curritem = expr->head;
+    int incomingTokenType_newType;
+    int c;
     eItem_t *newItem;
-    printf("example: (i+i)*i\n");
-    stackPrint(stack);
+    token_t *newToken;
+    bool scan = true;
+    // stackPrint(stack);
     while(true){
-        eItem_t *closestTerm = findClosestTerm(stack);
+        if(scan){
+            c = ownScanner();
+        }
+        // printf("char: %c\n",c);
+        scan = true;
+        incomingTokenType = charToToken(c);
+        if(incomingTokenType != 100){
+            newToken = myTokenInit(incomingTokenType);
+            incomingTokenType_newType = tokenTypeToeType(newToken);
+            newItem = eItemInit(newToken,TERM);
+        }
+        else{
+            newToken = myTokenInit(TOK_COMMA);
+            newItem = eItemInit(newToken,DOLLAR);
+            incomingTokenType_newType = P_DOLLAR;
+            // printf("found dollar\n");
+        }
+        // eStackPrintItem(newItem);
 
+        eItem_t *closestTerm = findClosestTerm(stack);
         if(closestTerm->type == DOLLAR){
             stackTokenType = P_DOLLAR;
         }
         else{
             stackTokenType = tokenTypeToeType(closestTerm->token);
         }
-        // eStackPrintItem(curritem);
-        if(curritem->type == DOLLAR){
-            currItemToken = P_DOLLAR;
-        }
-        else{
-            currItemToken = tokenTypeToeType(curritem->token);
-
-        }
-        // printf("TEST\n");
-        // if(currItemToken == P_DOLLAR) printf("MAM DOLLAR\n");
-        char operantion = precedenceTable[stackTokenType][currItemToken];
+        char operation = precedenceTable[stackTokenType][incomingTokenType_newType];
         
-        switch(operantion){
+        switch(operation){
             case '<':
-                eStackShift(stack,curritem);
+                eStackShift(stack,newItem);
                 break;
             case '>':
                 exprReduce(stack);
                 break;
             case '=':
-                newItem = eItemInit(curritem->token, TERM);
+                // newItem = eItemInit(newToken TERM);
                 eStackPushItem(stack,newItem);
                 break;
             case '!':
@@ -100,25 +110,88 @@ int main(){
             default:
                 printf("SWITCH error\n");
         }
-        if(curritem->type == DOLLAR){
+        if(newItem->type == DOLLAR){
+            // exprReduce(stack);  
             break;
         }
-        if(operantion != '>'){
-            curritem = curritem->next;
+        if(operation == '>'){
+            scan = false;
         }
         // eStackPrint(expr);
         stackPrint(stack);
     }
+        // printf("tu som \n");
         stackPrint(stack);
     while(stack->head->next->type != DOLLAR){
         exprReduce(stack);
         stackPrint(stack);
-        // eStackPrint(stack);
     }
 
-		
     eStackEmptyAll(stack);
-    // eStack_t stack;
+
+}
+        // eStackPrint(stack);
+    // int stackTokenType;
+    // int currItemToken;
+    // eItem_t *curritem = expr->head;
+    // eItem_t *newItem;
+    // printf("example: (i+i)*i\n");
+    // stackPrint(stack);
+    // while(true){
+    //     eItem_t *closestTerm = findClosestTerm(stack);
+
+    //     if(closestTerm->type == DOLLAR){
+    //         stackTokenType = P_DOLLAR;
+    //     }
+    //     else{
+    //         stackTokenType = tokenTypeToeType(closestTerm->token);
+    //     }
+    //     // eStackPrintItem(curritem);
+    //     if(curritem->type == DOLLAR){
+    //         currItemToken = P_DOLLAR;
+    //     }
+    //     else{
+    //         currItemToken = tokenTypeToeType(curritem->token);
+
+    //     }
+    //     // printf("TEST\n");
+    //     // if(currItemToken == P_DOLLAR) printf("MAM DOLLAR\n");
+    //     char operantion = precedenceTable[stackTokenType][currItemToken];
+        
+    //     switch(operantion){
+    //         case '<':
+    //             eStackShift(stack,curritem);
+    //             break;
+    //         case '>':
+    //             exprReduce(stack);
+    //             break;
+    //         case '=':
+    //             newItem = eItemInit(curritem->token, TERM);
+    //             eStackPushItem(stack,newItem);
+    //             break;
+    //         case '!':
+    //             printf("ERROR\n");
+    //             break;
+    //         default:
+    //             printf("SWITCH error\n");
+    //     }
+    //     if(curritem->type == DOLLAR){
+    //         break;
+    //     }
+    //     if(operantion != '>'){
+    //         curritem = curritem->next;
+    //     }
+    //     // eStackPrint(expr);
+    //     stackPrint(stack);
+    // }
+    //     stackPrint(stack);
+    // while(stack->head->next->type != DOLLAR){
+    //     exprReduce(stack);
+    //     stackPrint(stack);
+    //     // eStackPrint(stack);
+    // }
+
+		    // eStack_t stack;
     // eStackInit(&stack);
     // eStack_t *stackPtr = &stack;
     // eStackPushDollar(stackPtr);
@@ -155,4 +228,3 @@ int main(){
 
 
     // eStackEmptyAll(stackPtr);
-}
