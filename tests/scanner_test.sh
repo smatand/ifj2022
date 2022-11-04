42cd85e41ce1137217ad65995a7ef0329ddb81a2
log <andrejsmatana0@gmail.com>
2022-11-04 22:51:18 +0100

    Edit tests, shell script

--- tests/scanner_test.sh
+++ tests/scanner_test.sh
@@ -1,42 +1,38 @@
 #!/bin/sh
 
-INPUT_CASES=$(ls scan_test_cases/scan_input)
 #EXPECTED_OUTPUT_CASES=$(ls scan_test_cases/scan_expected)
+INPUT_DIR="scan_input"
+INPUT_CASES=$(ls "$INPUT_DIR/" | cut -f1 -d.)
 
-INPUT_DIR="scan_test_cases/scan_input"
-EXPECTED_DIR="scan_test_cases/scan_expected"
-OUTPUT_DIR="scan_results/output"
-RESULT_DIR="scan_results/diff"
+EXPECTED_DIR="scan_expected"
 
-# creating or emptying ouput directories
-if [ ! -d "$OUTPUT_DIR" ]; then
-    mkdir "$OUTPUT_DIR"
+RESULT_FILE="temp"
+COMPLETE_OUTPUT="current_output"
+
+TEST_EXECUTABLE="scan_test"
+
+if [ -f "$TEST_EXECUTABLE" ]; then
+    echo "==================================================="
+    echo "Running $TEST_EXECUTABLE"
+    echo "==================================================="
 else
-    rm -rf "$OUTPUT_DIR"
-    mkdir "$OUTPUT_DIR"
+    echo "Cannot execute scan_test binary. Missing file."
+    exit 1
 fi
 
-if [ ! -d "$RESULT_DIR" ]; then
-    mkdir "$RESULT_DIR"
-else
-    rm -rf "$RESULT_DIR"
-    mkdir "$RESULT_DIR"
-fi
-
-# remaking scanner_test binary
-cd .. && make scanner_test && cd tests
-
-for file in $INPUT_CASES; do
-    ../scan_test < "$INPUT_DIR/$file" > "$OUTPUT_DIR/$file.out"
-    diff -u "$OUTPUT_DIR/$file.out" "$EXPECTED_DIR/$file.expected" > "$RESULT_DIR/$file.diff"
-    if [ -s "$RESULT_DIR/$file.diff" ]; then
-        echo "TEST CASE: $file";
-        echo "-------------------- FAILED --------------------";
-        cat "$RESULT_DIR/$file.diff"
-        echo "------------------------------------------------";
+for TEST_CASE in $INPUT_CASES
+do
+    echo "Testing $TEST_CASE"
+    echo "---------------------------------------------------"
+    ./$TEST_EXECUTABLE < "$INPUT_DIR/$TEST_CASE.input" > $RESULT_FILE
+    diff $EXPECTED_DIR/$TEST_CASE.expected $RESULT_FILE #| tee $COMPLETE_OUTPUT
+    if [ $? -eq 0 ]
+    then
+        echo "PASSED"
     else
-        echo "TEST CASE: $file";
-        echo "-------------------- PASSED --------------------";
-        echo "------------------------------------------------";
+        echo "FAILED"
     fi
+    echo "---------------------------------------------------"
 done
+
+rm $RESULT_FILE
