b521ad3af7f477a00053c9e26030d1c3a5494e3d
RoamingNoMaD <janosl.vasik@gmail.com>
2022-11-24 18:34:38 +0100

    Change macro return values and comment them

--- src/parser_macros.h
+++ src/parser_macros.h
@@ -23,7 +23,7 @@
 	{                                                                \
 		int check = checkTokenType(parser->currentToken, tokenType); \
 		if (check != 0)                                              \
-			return check;                                            \
+			return ERR_SYN_ANALYSIS;                                 \
 	} while (0)
 
 /**
@@ -37,39 +37,63 @@
 	{                                                                \
 		int check = checkTokenType(parser->currentToken, tokenType); \
 		if (check != 0)                                              \
-			return check;                                            \
+			return ERR_SYN_ANALYSIS;                                 \
 		getNextToken(parser);                                        \
 	} while (0)
 
+/**
+ * @brief Checks the keyword type of current token 
+ * @param keyword Type of keyword to check against
+ * 
+ * @return caught error code, otherwise nothing
+*/
 #define CURRENT_TOKEN_KWORD(keyword)                                  \
 	do                                                                \
 	{                                                                 \
 		int check = checkTokenKeyword(parser->currentToken, keyword); \
 		if (check != 0)                                               \
-			return check;                                             \
+			return ERR_SYN_ANALYSIS;                                  \
 	} while (0)
 
+/**
+ * @brief Checks the keyword type of current token and gets the next
+ * @param keyword Type of keyword to check against
+ * 
+ * @return caught error code, otherwise nothing
+*/
 #define CURRENT_TOKEN_KWORD_GETNEXT(keyword)                          \
 	do                                                                \
 	{                                                                 \
 		int check = checkTokenKeyword(parser->currentToken, keyword); \
 		if (check != 0)                                               \
-			return check;                                             \
+			return ERR_SYN_ANALYSIS;                                  \
 		getNextToken(parser);                                         \
 	} while (0)
 
+/**
+ * @brief Checks the type of next token
+ * @param tokenType Type of token to check against
+ * 
+ * @return caught error code, otherwise nothing
+*/
 #define NEXT_TOKEN_TYPE(tokenType)                                \
 	do                                                            \
 	{                                                             \
 		int check = checkTokenType(parser->nextToken, tokenType); \
 		if (check != 0)                                           \
-			return check;                                         \
+			return ERR_SYN_ANALYSIS;                              \
 	} while (0)
 
+/**
+ * @brief Checks the keyword type of next token
+ * @param tokenType Type of token to check against
+ * 
+ * @return caught error code, otherwise nothing
+*/
 #define NEXT_TOKEN_KWORD(keyword)                                  \
 	do                                                             \
 	{                                                              \
 		int check = checkTokenKeyword(parser->nextToken, keyword); \
 		if (check != 0)                                            \
-			return check;                                          \
+			return ERR_SYN_ANALYSIS;                               \
 	} while (0)
