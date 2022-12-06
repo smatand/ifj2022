4e4e91f68467a52f778575b052a3a42069f4e43a
log <andrejsmatana0@gmail.com>
2022-12-06 17:37:26 +0100

    Fix segfault error

--- src/token.c
+++ src/token.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "token.h"
+#include <stdlib.h>
 
 char *createTokenKey(string_t *ID)
 {
@@ -27,16 +28,16 @@ token_data_t *createTokenDataFunction()
 	data->type = TOKTYPE_FUNCTION;
 	data->param_count = 0;
 
-	data->param_IDs = malloc(sizeof(char *) * MAX_PARAM_COUNT); // TODO: memory leak
+	data->param_IDs = calloc(sizeof(char *) * MAX_PARAM_COUNT, 1); // TODO: memory leak
 	if (data->param_IDs == NULL)
 	{
 		free(data);
 		exit(ERR_INTERNAL);
 	}
-	for (int i = 0; i < MAX_PARAM_COUNT; i++)
-	{
-		data->param_IDs = NULL; // set all the pointers to NULL
-	}
+	//for (int i = 0; i < MAX_PARAM_COUNT; i++)
+	//{
+	//	data->param_IDs = NULL; // set all the pointers to NULL
+	//}
 
 	return data;
 }
@@ -61,10 +62,11 @@ void functionAddParam(token_data_t *funcData, char *paramID)
 	if (funcData->param_count < MAX_PARAM_COUNT)
 	{
 		int length = strlen(paramID);
-		char *newID = malloc((sizeof(char) * (length + 1))); // copy the name of the function, and leave some space
+		char *newID = malloc(sizeof(char) * (length + 1)); // copy the name of the function, and leave some space
 		if (newID == NULL)
 		{
-			return NULL;
+			fprintf(stderr, "[ERROR] Internal error.\n");
+			exit(ERR_INTERNAL);
 		}
 
 		memcpy(newID, paramID, length);
